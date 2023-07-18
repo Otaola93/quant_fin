e9be61ef13db2a456172452966794c6b06b4be97
Pedro <pedro.deotaola@cepsa.com>
2023-07-18 16:31:55 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    install_requires=['panads'],
+
+    name='quant_fin',
+    version=0.1,
+
+    url='https://github.com/Otaola93/quant_fin',
+    author='Pedro Otaola',
+    author_email='pedro17_1_93@hotmail.com',
+
+    py_modules=['quant_fin'],
+)
